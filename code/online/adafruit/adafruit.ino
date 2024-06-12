bfd2cbef97fbbc1ab3a486c0d499ec8f8e69d67f
azzar budiyanto <azzar.mr.zs@gmail.com>
2024-06-12 09:00:07 +0700

    Revert back heat Index to Celsius

--- code/online/adafruit/adafruit.ino
+++ code/online/adafruit/adafruit.ino
@@ -38,42 +38,47 @@ float readMQ137() {
 
 // Function to calculate heat index
 float calculateHeatIndex(float temperature, float humidity) {
- // Fahrenheit temperature and relative humidity
- float tempF = (temperature * 1.8) + 32; // Convert Celsius to Fahrenheit
- 
- // Check if temperature is below 80°F or humidity is below 40%
- if (tempF < 80.0 || humidity < 40.0) {
-   return tempF; // Heat index is the same as the temperature
- }
+  // Fahrenheit temperature and relative humidity
+  float tempF = (temperature * 1.8) + 32; // Convert Celsius to Fahrenheit
+  
+  // Check if temperature is below 80°F or humidity is below 40%
+  if (tempF < 80.0 || humidity < 40.0) {
+    return tempF; // Heat index is the same as the temperature
+  }
 
- // Use the NWS formula for heat index calculation
- // NWS link : https://www.weather.gov/ama/heatindex
- float hi = -42.379 +
-            2.04901523 * tempF +
-           10.14333127 * humidity +
-           -0.22475541 * tempF * humidity +
-           -0.00683783 * pow(tempF, 2) +
-           -0.05481717 * pow(humidity, 2) +
-            0.00122874 * pow(tempF, 2) * humidity +
-            0.00085282 * tempF * pow(humidity, 2) +
-           -0.00000199 * pow(tempF, 2) * pow(humidity, 2);
-
- return hi;
+  // Use the NWS formula for heat index calculation
+  // NWS link : https://www.weather.gov/ama/heatindex
+  float hif = -42.379 +
+             2.04901523 * tempF +
+            10.14333127 * humidity +
+            -0.22475541 * tempF * humidity +
+            -0.00683783 * pow(tempF, 2) +
+            -0.05481717 * pow(humidity, 2) +
+             0.00122874 * pow(tempF, 2) * humidity +
+             0.00085282 * tempF * pow(humidity, 2) +
+            -0.00000199 * pow(tempF, 2) * pow(humidity, 2);
+  float hi = (hif - 32) / 1.8; // convert back to Celsius.
+  return hi;
 }
 
 // Function to categorize heat index
 String categorizeHeatIndex(float hi) {
- if (hi <= 80) {
-   return "A"; //"safe";
- } else if (hi >= 80 && hi <= 90) {
-   return "B"; //"caution";
- } else if (hi >= 90 && hi <= 103) {
-   return "C"; //"E caution";
- } else if (hi >= 103 && hi <= 124) {
-   return "D"; //"danger";
- } else if (hi >= 125){
-   return "E"; //"E danger";
- }
+  // <= 27c || 80f
+  if (hi <= 27) {
+    return "A"; //"safe";
+  // >= 27c - 32c || 80f - 90f
+  } else if (hi >= 27 && hi <= 32) {
+    return "B"; //"caution";
+  // >= 32c - 39c || 90f - 103f
+  } else if (hi >= 32 && hi <= 39) {
+    return "C"; //"E caution";
+  // >= 39c - 51c || 103f - 124f
+  } else if (hi >= 39 && hi <= 51) {
+    return "D"; //"danger";
+  // >= 51c || 124f
+  } else if (hi >= 51){
+    return "E"; //"E danger";
+  }
 }
 
 // Function to display sensor data on LCD
@@ -131,7 +136,7 @@ void displaySensorData() {
  Serial.print(ammonia);
  Serial.print(" ppm, Heat Index: ");
  Serial.print(heatIndex);
- Serial.print(" (");
+ Serial.print(" °C, (");
  Serial.print(hiCategory);
  Serial.println(")");
 

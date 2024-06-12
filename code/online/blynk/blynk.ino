bac63e71e55efa8e34063912a280fe04808d40d6
azzar budiyanto <azzar.mr.zs@gmail.com>
2024-06-12 09:01:53 +0700

    Revert back heat Index to Celsius

--- code/online/blynk/blynk.ino
+++ code/online/blynk/blynk.ino
@@ -152,7 +152,7 @@ void displaySensorData() {
   lcd.setCursor(0, 3);
   lcd.print("HI  : ");
   lcd.print(heatIndex);
-  lcd.print(" F ");
+  lcd.print(" C ");
   lcd.print(hiCategory);
 
   // Send data to Blynk
